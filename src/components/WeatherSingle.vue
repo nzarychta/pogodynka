88be5f7fe58ce8dcbf980abecc33af237d7649a8
nzarychta <natalia225@op.pl>
2022-04-28 18:25:54 +0200

    WheatherSingle

---
+++ src/components/WeatherSingle.vue
@@ -0,0 +1,42 @@
+<template>
+  
+      <table BORDER="1">
+    <tr>
+        <th>DATE</th>
+        <td>{{current.dt}}</td>
+    </tr> 
+      <tr>
+        <th>TEMP</th>
+        <td>{{current.temp}}</td>
+    </tr>
+        <tr>
+        <th>DESC</th>
+        <td>{{current.weather?.[0].description}}</td>
+
+        
+    </tr> 
+      <tr>
+          <th>ICON</th>
+          
+          <img 
+          v-if="current.weather?.[0].icon"
+          :src ="'http://openweathermap.org/img/wn/'+current.weather?.[0].icon+'@2x.png'" alt="Ikona"> 
+          
+          
+        </tr>
+</table>
+  
+</template>
+
+<script>
+export default {
+    props:{
+        current: Object
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/WeatherView.vue
+++ src/components/WeatherView.vue
@@ -1,19 +1,81 @@
 <template>
 <div class="weatherView">
 <h1>Weather</h1>
-{{weather}}
+
+
+
+<div class="cityPicker">
+    <input type="text" v-model='cityName'>
+    <button @click="getLocation">Szukaj</button>
+</div>
+
+
+<div class="currentWeather">
+    <h2>CURRENT WEATHER</h2>
+    <WeatherSingle :current = "current"/>
+</div>
+
+<div class="dailyWeather">
+   
+    <h2>DAILY WEATHER</h2>
+
+</div>
 </div>
 </template>
 
 <script>
+import WeatherSingle from'./WeatherSingle.vue'
 export default {
     name:'WeatherView',
+    components:{
+      WeatherSingle: WeatherSingle
+    },
     data(){
     return{
-    weather: "sun",
+        cityName:"Gdańsk",
+        lat:"",
+        lon:"",
+        current:{},
+        daily:[],
+        currentCity:"",
+    
 
     }
 
+},
+methods:{
+    getWeather(lat,lon){
+      console.log("getWeather");
+      fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&appid=04d03c358e8933ac6823da54c340c97b")
+      .then(dt=>dt.json())
+      .then(dt =>{
+      console.log(dt)
+      this.dily = dt.daily;
+      this.current = dt.current;
+    })
+    },
+    getLocation(){
+    
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+this.cityName+"&appid=04d03c358e8933ac6823da54c340c97b")
+    .then(dt=> dt.json())
+
+    .then(dt=>{
+      this.cityName=dt[0].name;
+      this.currentCity = dt[0].name;
+      this.lat=dt[0].lat;
+      this.lon=dt[0].lon;
+      this.getWeather(dt[0].lat, dt[0].lon);  
+    })
+
+    }
+
+},
+
+
+
+
+created(){
+    this.getLocation();
 }
 }
 </script>
