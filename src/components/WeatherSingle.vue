81f7bc3f18a07f9b1e7b59036b9204da10f32e54
nzarychta <natalia225@op.pl>
2022-05-04 18:29:34 +0200

    problem

--- src/components/WeatherSingle.vue
+++ src/components/WeatherSingle.vue
@@ -1,42 +1,58 @@
 <template>
-  
-      <table BORDER="1">
-    <tr>
-        <th>DATE</th>
-        <td>{{current.dt}}</td>
-    </tr> 
-      <tr>
-        <th>TEMP</th>
-        <td>{{current.temp}}</td>
-    </tr>
-        <tr>
-        <th>DESC</th>
-        <td>{{current.weather?.[0].description}}</td>
-
-        
-    </tr> 
-      <tr>
-          <th>ICON</th>
-          
-          <img 
-          v-if="current.weather?.[0].icon"
-          :src ="'http://openweathermap.org/img/wn/'+current.weather?.[0].icon+'@2x.png'" alt="Ikona"> 
-          
-          
-        </tr>
-</table>
-  
+  <div class="item">
+    <div class="date">{{unixToHumanDate(current.dt)}}</div>
+    <div class="temp">{{simplifyTemp(current.temp)}}°C</div>
+    <div class="icon">
+      <img
+        v-if="current.weather?.[0].icon"
+        :src="'http://openweathermap.org/img/wn/'+current.weather?.[0].icon+'@2x.png'"
+        alt="Ikona pogody">
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props:{
-        current: Object
+  props: {
+    current: Object
+  },
+  methods: {
+    unixToHumanDate( unixDate ){
+      const dt = new Date( unixDate * 1000)
+      return dt.toLocaleDateString()
+    },
+    simplifyTemp( temp ){
+      if(typeof temp != "object") return temp;
+      return temp.min+" - "+temp.max;
     }
-
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+  .item{
+      //border: 2px dashed red;//
+      display: flex;
+      text-align: center;
+      background: rgb(149, 149, 240);
+      border-radius: 10px ;
+      padding: .5em;
+      font-weight:bold;
+   .temp{
+    color: yellow;
+    text-align: flex;
+   }  
+   .date{
+     color: green;
+     text-align: center;
+     display: flex;
+     justify-content: center;
+   }  
+   
 
+   
+  }
+  
 </style>
+
+
--- src/components/WeatherView.vue
+++ src/components/WeatherView.vue
@@ -1,85 +1,87 @@
 <template>
-<div class="weatherView">
-<h1>Weather</h1>
+  <div class="weatherView">
+    <h1>Weather</h1>
+    
 
+    <div class="cityPicker">
+      <input type="text" v-model="cityName">
+      <button @click="getLocation">SZUKAJ</button>
+    </div>
 
+    <div class="currentWeather">
+      <h2>CURRENT WEATHER</h2>
+      <WeatherSingle :current="current" />
+    </div>
 
-<div class="cityPicker">
-    <input type="text" v-model='cityName'>
-    <button @click="getLocation">Szukaj</button>
-</div>
+    <div class="dailyWeather">
+      <h2>DAILY WEATHER</h2>
+      <div class="dailyWrapper">        
+        <div class="daily" v-for="day in daily" :key="day.dt">
+          <WeatherSingle :current="day" />
+        </div>
+      </div>
+    </div>
 
-
-<div class="currentWeather">
-    <h2>CURRENT WEATHER</h2>
-    <WeatherSingle :current = "current"/>
-</div>
-
-<div class="dailyWeather">
-   
-    <h2>DAILY WEATHER</h2>
-
-</div>
-</div>
+  </div>
 </template>
 
 <script>
-import WeatherSingle from'./WeatherSingle.vue'
+import WeatherSingle from './WeatherSingle.vue'
 export default {
-    name:'WeatherView',
-    components:{
-      WeatherSingle: WeatherSingle
-    },
-    data(){
+  name: 'WeatherView',
+  components: {
+    WeatherSingle: WeatherSingle
+  },
+  data(){
     return{
-        cityName:"Gdańsk",
-        lat:"",
-        lon:"",
-        current:{},
-        daily:[],
-        currentCity:"",
-    
-
+      cityName: "Gdańsk",
+      lat: "",
+      lon: "",
+      current: {},
+      daily: [],
+      currentCity: "",
     }
-
-},
-methods:{
-    getWeather(lat,lon){
-      console.log("getWeather");
-      fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&appid=04d03c358e8933ac6823da54c340c97b")
-      .then(dt=>dt.json())
-      .then(dt =>{
-      console.log(dt)
-      this.dily = dt.daily;
-      this.current = dt.current;
-    })
+  },
+  methods:{
+    getWeather(lat, lon){
+      fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&units=metric&appid=04d03c358e8933ac6823da54c340c97b")
+      .then(dt => dt.json())
+      .then(dt => {
+        console.log(dt)
+        this.daily = dt.daily;
+        this.current = dt.current;
+      })
     },
     getLocation(){
-    
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+this.cityName+"&appid=04d03c358e8933ac6823da54c340c97b")
-    .then(dt=> dt.json())
-
-    .then(dt=>{
-      this.cityName=dt[0].name;
-      this.currentCity = dt[0].name;
-      this.lat=dt[0].lat;
-      this.lon=dt[0].lon;
-      this.getWeather(dt[0].lat, dt[0].lon);  
-    })
-
+      fetch("http://api.openweathermap.org/geo/1.0/direct?q="+this.cityName+"&appid=04d03c358e8933ac6823da54c340c97b")
+      .then(dt => dt.json())
+      .then(dt => {
+        this.cityName = dt[0].name;
+        this.currentCity = dt[0].name;
+        this.lat = dt[0].lat;
+        this.lon = dt[0].lon;
+        this.getWeather(dt[0].lat, dt[0].lon);
+      })
     }
-
-},
-
-
-
-
-created(){
+  },
+  created(){
     this.getLocation();
-}
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+  
 
+  .currentWeather{
+      flex-direction: column;
+      align-items: center;
+    }
+
+    .dailyWrapper,.currentWeather{
+      display:flex;
+      justify-content: center;
+      flex-wrap:wrap;
+    }
+  
 </style>
